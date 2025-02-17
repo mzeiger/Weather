35649b9c7030b60122b5de77be7b17095d93c6f2
markz <markz@AX8_Pro>
2025-02-17 15:04:53 -0700

    Gave GPS page a background image and foxed precision of data

--- lib/pages/gps_page.dart
+++ lib/pages/gps_page.dart
@@ -36,6 +36,10 @@ class _GpsPageState extends State<GpsPage> {
     _gps.stopPositionStream();
   }
 
+  String metersToFeet(double meters) {
+    return (meters * 3.28084).toStringAsFixed(4);
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget? childWidget;
@@ -51,25 +55,58 @@ class _GpsPageState extends State<GpsPage> {
         appBar: AppBar(
           title: const Text('GPS'),
         ),
-        body: Center(
-          child: Container(
-            child: childWidget,
+        body: DecoratedBox(
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/images/pexels-pixabay-87651.jpg'),
+                  fit: BoxFit.cover)),
+          child: Center(
+            child: Container(
+              child: childWidget,
+            ),
           ),
         ));
   }
 
   Widget showPosition() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Text('Longitude: ${_userPosition!.longitude.toString()}'),
-        Text('Latitude: ${_userPosition!.latitude.toString()}'),
-        Text('Altitude: ${_userPosition!.altitude.toString()}'),
-        Text('Accuracy: ${_userPosition!.accuracy.toString()}'),
-        Text('Alt. Accuracy: ${_userPosition!.altitudeAccuracy.toString()}'),
-        Text('Speed: ${_userPosition!.speed.toStringAsFixed(2)}'),
-      ],
+    return Container(
+      height: MediaQuery.of(context).size.height * .3,
+      width: MediaQuery.of(context).size.width * .8,
+      // color: const Color.fromARGB(200, 228, 206, 206),
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(
+          Radius.circular(20),
+        ),
+        color: Color.fromARGB(160, 228, 190, 190),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'Longitude: ${_userPosition!.longitude.toStringAsFixed(6)}',
+            style: _textStyle(),
+          ),
+          Text('Latitude: ${_userPosition!.latitude.toStringAsFixed(6)}',
+              style: _textStyle()),
+          Text('Altitude: ${metersToFeet(_userPosition!.altitude)}',
+              style: _textStyle()),
+          Text('Accuracy: ${_userPosition!.accuracy.toStringAsFixed(4)}',
+              style: _textStyle()),
+          Text(
+              'Alt. Accuracy: ${_userPosition!.altitudeAccuracy.toStringAsFixed(4)}',
+              style: _textStyle()),
+          Text('Speed: ${_userPosition!.speed.toStringAsFixed(4)}',
+              style: _textStyle()),
+        ],
+      ),
     );
+  }
+
+  TextStyle _textStyle() {
+    return const TextStyle(
+        fontWeight: FontWeight.bold,
+        color: Color.fromARGB(255, 70, 54, 6),
+        fontSize: 14);
   }
 }
